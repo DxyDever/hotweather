cf0f17c1c4f03b04974a6e96b5405f9fac2f68de
deng <1144513769@qq.com>
2019-12-15 22:54:20 +0800

    加入了显示天气信息的功能

--- app/src/main/java/com/example/hotweather/db/City.java
+++ app/src/main/java/com/example/hotweather/db/City.java
@@ -4,7 +4,7 @@ import android.print.PrinterId;
 
 import org.litepal.crud.DataSupport;
 
-public class City extends DataSupport {
+/*public class City extends DataSupport {
     private int id;
 
     public int getId() {
@@ -44,5 +44,92 @@ public class City extends DataSupport {
     }
 
     private int provinceId;
+
+}
+*/
+
+public class City extends DataSupport {
+
+
+
+    private int id;
+
+
+
+    private String cityName;
+
+
+
+    private int cityCode;
+
+
+
+    private int provinceId;
+
+
+
+    public int getId() {
+
+        return id;
+
+    }
+
+
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getCityName() {
+
+        return cityName;
+
+    }
+
+
+
+    public void setCityName(String cityName) {
+
+        this.cityName = cityName;
+
+    }
+
+
+
+    public int getCityCode() {
+
+        return cityCode;
+
+    }
+
+
+
+    public void setCityCode(int cityCode) {
+
+        this.cityCode = cityCode;
+
+    }
+
+
+
+    public int getProvinceId() {
+
+        return provinceId;
+
+    }
+
+
+
+    public void setProvinceId(int provinceId) {
+
+        this.provinceId = provinceId;
+
+    }
+
+
 
 }
--- app/src/main/java/com/example/hotweather/db/Province.java
+++ app/src/main/java/com/example/hotweather/db/Province.java
@@ -2,7 +2,7 @@ package com.example.hotweather.db;
 
 import org.litepal.crud.DataSupport;
 
-public class Province extends DataSupport {
+/*public class Province extends DataSupport {
     private int id;
 
     public String getProvinceName() {
@@ -34,5 +34,70 @@ public class Province extends DataSupport {
 
 
 
+
+}
+*/
+
+public class Province extends DataSupport {
+
+
+
+    private int id;
+
+
+
+    private String provinceName;
+
+
+
+    private int provinceCode;
+
+
+
+    public int getId() {
+
+        return id;
+
+    }
+
+
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getProvinceName() {
+
+        return provinceName;
+
+    }
+
+
+
+    public void setProvinceName(String provinceName) {
+
+        this.provinceName = provinceName;
+
+    }
+
+
+
+    public int getProvinceCode() {
+
+        return provinceCode;
+
+    }
+
+
+
+    public void setProvinceCode(int provinceCode) {
+
+        this.provinceCode = provinceCode;
+
+    }
 
 }
